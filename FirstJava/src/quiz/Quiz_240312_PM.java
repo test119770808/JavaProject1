8c942ca24c689196ecbc2017ea92d707b685106f
test119770808 <test119770808@gmail.com>
2024-03-12 18:17:16 +0900

    240312-13오전 문제풀이

--- FirstJava/src/quiz/Quiz_240312_PM.java
+++ FirstJava/src/quiz/Quiz_240312_PM.java
@@ -17,6 +17,30 @@ public class Quiz_240312_PM {
 		//   앞서 배운 난수를 활용하여, 중복되는 번호 없이 1 ~ 45까지의 숫자 중
 		//   6개의 숫자를 랜덤추출하고, 콘솔에 출력하세요. 
 		//   (while문에 for)
+		System.out.println("======== 로또 추출 ========");
+		int[] lotto = {0,0,0,0,0,0};
+		
+		int index = 0;
+		while(true) {
+			// 랜덤값 생성
+			int rand = (int)(Math.random()*45) + 1;  // 1 ~ 45
+			int i = 0; //index와 값을 비교하기 위해서... 
+			for (i = 0; i < index; i++) {
+				if(rand == lotto[i]) {  // 추첨된 번호가 중복됨...
+					break;
+				}
+			}
+			if(index == i) {  // 추첨된 번호가 lotto에 없어요... 
+				lotto[index++] = rand;				
+			}
+			if(index > 5) break; // 6개 번호 추출 끝
+		}
+		
+		// 결과 추출
+		for (int i=0; i< lotto.length; i++) {
+			System.out.print(lotto[i]+"\t");
+		}
+		
 		
 		// 4. 대문자가 입력된다면 소문자로 출력하고, 
 		//   소문자가 입력된다면 대문자로 출력하는 프로그램을 작성해보세요
