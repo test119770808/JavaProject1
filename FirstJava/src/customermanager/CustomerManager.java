7d697fb9cccc09fd84ebac9f5a9de3361dde3fcf
test119770808 <test119770808@gmail.com>
2024-03-13 16:25:43 +0900

    240313 - 4:20

---
+++ FirstJava/src/customermanager/CustomerManager.java
@@ -0,0 +1,13 @@
+package customermanager;
+
+public class CustomerManager {
+
+	public static void main(String[] args) {
+		// 이전에 만들어 놓은 고객관리 프로그램을 OOP형식으로 변경하여 재 프로그래밍을 진행!
+		// 1. Customer 클래스를 생성하고,
+		// 2. 관련 프로그램을 위에 생성한 Customer 클래스 기준으로 재작성하세요!
+		
+
+	}
+
+}
