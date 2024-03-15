62e7d1df7f5b8c631a8552005612b5d14db180cc
test119770808 <test119770808@gmail.com>
2024-03-15 11:13:51 +0900

    240315 - 2교시

--- FirstJava/src/C06_static.java
+++ FirstJava/src/C06_static.java
@@ -26,12 +26,35 @@
  * 
  */
 
+import obj.Count;
 
 public class C06_static {
 
 	public static void main(String[] args) {
 		
-
+		Count c1 = new Count();
+		c1.a++;
+		c1.b++;  //경고 : The static field Count.b should be accessed in a static way
+		System.out.println("Non-static a : "+c1.a);
+		System.out.println("static b : "+c1.b);
+		
+		Count c2 = new Count();
+		c2.a++;
+		c2.b++;
+		System.out.println("Non-static a : "+c2.a);
+		System.out.println("static b : "+c2.b);
+		
+		Count.b++;
+		System.out.println("c1.b : "+c1.b);
+		System.out.println("c2.b : "+c2.b);
+		System.out.println("Count.b : "+Count.b);
+		
+		// static method
+		System.out.println("Count.doIt () : "+Count.doIt());
+		System.out.println("Count.doIt () : "+Count.doIt());
+		System.out.println("Count.doIt () : "+Count.doIt());
+		
+		
 	}
 
 }
--- FirstJava/src/obj/Count.java
+++ FirstJava/src/obj/Count.java
@@ -6,8 +6,10 @@ public class Count {
 	public static int b = 0;
 	
 	public static int doIt() {
-		return ++a;
-//		return ++b;
+//		return ++a;   // Cannot make a static reference to the non-static field a
+		// static 메서드는 인스턴스 변수(a)를 객체 생성 없이 참조 불가능
+		++new Count().a; // ++((new Count().a)
+		return ++b;
 	}
 
 }
