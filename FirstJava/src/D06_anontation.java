052cacdb8225416c7ff8abd614bfefa8ac736320
test119770808 <test119770808@gmail.com>
2024-03-19 14:27:00 +0900

    240319 4교시

--- FirstJava/src/D06_anontation.java
+++ FirstJava/src/D06_anontation.java
@@ -16,7 +16,7 @@ class UserClass {
 
 public class D06_anontation {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception  {
 		Method method[] = UserClass.class.getDeclaredMethods();
 		System.out.println("method[]에 있는 내용의 갯수 : "+method.length);
 		for (int i = 0; i < method.length; i++) {
