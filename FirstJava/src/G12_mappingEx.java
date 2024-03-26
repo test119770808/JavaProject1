ac9fe5ce62df9a0ec22d12444da334fbef1d361e
test119770808 <test119770808@gmail.com>
2024-03-26 15:21:05 +0900

    240326 5교시

--- FirstJava/src/G12_mappingEx.java
+++ FirstJava/src/G12_mappingEx.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 public class G12_mappingEx {
 	
@@ -35,10 +36,46 @@ public class G12_mappingEx {
 		dsr.forEach(n -> System.out.println(n));
 		System.out.println();
 		
+		// 문자열을 , 로 분리하여 int 자료형으로 변환해서 매핑
+		List<String> list3 = Arrays.asList("1, 2, 3","4, 5, 6");
+		IntStream isr = list3.stream().flatMapToInt(data -> {
+			String[] strArr = data.split(",");
+			int[] intArr = new int[strArr.length];
+			for(int i = 0; i< strArr.length; i++) {
+				intArr[i] = Integer.parseInt(strArr[i].trim());
+			}
+			return Arrays.stream(intArr);
+		});
+		isr.forEach(s -> System.out.println(s));
 		
+		//mapXXX() 메서드 : 요소를 새로 구성해서 새로운 스트림을 리턴
+		List<String> list4 = Arrays.asList("동해물과", "백두산이","마르고 닳도록");
 		
+		System.out.println("함수적 인터페이스 방식");
+		list4.stream().mapToInt(s -> s.length())
+					.forEach(len -> System.out.println(len));
+		System.out.println();
 		
+		System.out.println("메서드 참조 방식");
+		list4.stream().mapToInt(String::length)
+		 			.forEach(len -> System.out.println(len));
 
+		// - asDoubleStream, asLongStream, boxed
+		// asDoubleStream : IntStream, LongStream을 DoubleStream으로 형변환
+		// asLongStream : IntStream을 LongStream으로 형변환
+		// boxed : IntStream, LongStream, DoubleStream을 각가 Stream<Integer>
+		//     , Stream<Long>, Stream<Double>로 형변환
+		int intArr[] = {10, 20, 30, 40, 50, 60};
+		
+		// double타입의 요소를 갖는 DoubleStream으로 형변환
+		Arrays.stream(intArr).asDoubleStream()
+						.forEach(d -> System.out.println(d));
+		System.out.println();
+		
+		//int타입의 요소를 갖는  Stream<Integer>으로 형변환
+		Arrays.stream(intArr).boxed()
+						.forEach(i -> System.out.println(i.getClass()));
+		
 	}
 
 }
