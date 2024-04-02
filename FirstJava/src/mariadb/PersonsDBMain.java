e2d4ce2d81fd5d6e0d94f9c6f8a8d8b875c5e701
test119770808 <test119770808@gmail.com>
2024-04-02 14:47:20 +0900

    240402 5교시

--- FirstJava/src/mariadb/PersonsDBMain.java
+++ FirstJava/src/mariadb/PersonsDBMain.java
@@ -1,5 +1,6 @@
 package mariadb;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class PersonsDBMain {
@@ -46,22 +47,69 @@ public class PersonsDBMain {
 	}
 	
 	static void selectAll() {
+		PersonsDAO dao = new PersonsDAO();
+		List<PersonsVO> list = dao.selectAll();
+		for (PersonsVO vo : list) {
+			System.out.println(vo);
+		}
 		
 	}
 	
 	static void selectOne() {
-		
+		PersonsDAO dao = new PersonsDAO();
+		System.out.print("출력할 레코드의 ID를 입력하세요 : ");
+		int id = scan.nextInt();
+		PersonsVO vo = dao.selectOne(id);
+		System.out.println(vo);
 	}
 	
 	static void insert() {
+		PersonsDAO dao = new PersonsDAO();
+		PersonsVO vo = new PersonsVO();
+		System.out.println("레코드 추가하기 !!!");
 		
+		System.out.print("lastName(성) 입력 : ");
+		vo.setLastName(scan.next());
+		System.out.print("firstName(이름) 입력 : ");
+		vo.setFirstName(scan.next());
+		System.out.print("age(나이) 입력 : ");
+		vo.setAge(scan.nextInt());
+		System.out.print("city(도시) 입력 : ");
+		vo.setCity(scan.next());
+		
+		int result = dao.insertPersons(vo);
+		if(result != 0) System.out.println("레코드 추가를 성공했습니다.");
+		else System.out.println("레코드 추가를 실패했습니다.");
 	}
 	
 	static void update() {
 		
+		PersonsDAO dao = new PersonsDAO();
+		PersonsVO vo = new PersonsVO();
+		System.out.println("레코드 수정하기 !!!");
+		
+		System.out.print("수정할 레코드 id를 입력하세요 : ");
+		vo = dao.selectOne(scan.nextInt());
+		
+		System.out.print("lastName("+vo.getLastName()+") 수정 : ");
+		vo.setLastName(scan.next());
+		System.out.print("firstName("+vo.getFirstName()+") 수정 : ");
+		vo.setFirstName(scan.next());
+		System.out.print("age("+vo.getAge()+") 수정 : ");
+		vo.setAge(scan.nextInt());
+		System.out.print("city("+vo.getCity()+") 수정 : ");
+		vo.setCity(scan.next());
+		
+		PersonsDAO dao2 = new PersonsDAO();
+		int result = dao2.updatePersons(vo);
+		if(result != 0) System.out.println("레코드 수정에 성공했습니다.");
+		else System.out.println("레코드 수정에 실패했습니다.");
 	}
 	static void delete() {
-		
+		PersonsDAO dao = new PersonsDAO();
+		System.out.print("삭제할 ID를 입력하세요 : ");
+		dao.deletePersons(scan.nextInt());
+		System.out.println("삭제했습니다.");
 	}
 
 }
