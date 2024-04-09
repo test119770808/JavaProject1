f9f90f642b4d8048a9b6cd5292ab3a793893b454
test119770808 <test119770808@gmail.com>
2024-04-09 15:38:52 +0900

    240409 6교시

--- jspbasic/src/main/java/com/servlet/Forward.java
+++ jspbasic/src/main/java/com/servlet/Forward.java
@@ -1,6 +1,8 @@
 package com.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,7 +30,9 @@ public class Forward extends HttpServlet {
 		// request에 1회성으로 사용하기 위해서 값을 강제로 저장
 		// setAttribute("변수명", 값)
 		request.setAttribute("name", "홍길동");
-		response.sendRedirect("actionTag/forward_ex04.jsp");
+//		response.sendRedirect("actionTag/forward_ex04.jsp");
+		RequestDispatcher dp = request.getRequestDispatcher("actionTag/forward_ex04.jsp");
+		dp.forward(request, response);  //자바에서의 forward 방식... 
 		
 		
 		
