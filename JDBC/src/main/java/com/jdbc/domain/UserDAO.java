c4229c4b56dd4dbe7844bab456a0a4580bb8ea66
test119770808 <test119770808@gmail.com>
2024-04-12 17:25:52 +0900

    240412 7교시

--- JDBC/src/main/java/com/jdbc/domain/UserDAO.java
+++ JDBC/src/main/java/com/jdbc/domain/UserDAO.java
@@ -6,11 +6,16 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 public class UserDAO {
 	
 	// 싱글톤 패턴은 하나의 객체만 생성하여 사용하도록 만드는 패턴 - 객체를 1개로 제한.
 	// 여러개의 객체가 생성되게하면 메모리 과부하가 올 수 있는 경우에 사용. 
 	// DAO객체는 DB연동을 담당하는 클래스로 싱글톤 방식으로 생성... 
+	private DataSource ds;
 	
 	// 1. 스스로의 객체를 멤버변수로 선언하고 1개로 제한. 
 	private static UserDAO instance = new UserDAO();
@@ -19,9 +24,11 @@ public class UserDAO {
 	private UserDAO() {
 		// 생성자가 한번 동작할 때 다음과 같은 동작 처리... 드라이버와 연결... 
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 호출시 에러 발생");
+			// 커넥션 풀을 꺼내는 작업
+			InitialContext ctx = new InitialContext(); //초기 설정파일 저장되는 객체
+			ds = (DataSource)ctx.lookup("java:comp/env/jdbc/mysql");
+		} catch (NamingException e) {
+			System.out.println("커넥션 풀링 에러 발생");
 		}
 	}
 	
@@ -32,9 +39,10 @@ public class UserDAO {
 	
 	
 	//------------- 중복되는 코드를 멤버변수로 선언----------
-	private String url = "jdbc:mysql://localhost:3306/jdbctest?serverTimezone=Asia/Seoul";
-	private String user = "jdbc";
-	private String password = "jdbc";
+//	private String url = "jdbc:mysql://localhost:3306/jdbctest?serverTimezone=Asia/Seoul";
+//	private String user = "jdbc";
+//	private String password = "jdbc";
+	
 	
 	private Connection conn = null;
 	private PreparedStatement pstmt = null;
@@ -49,7 +57,8 @@ public class UserDAO {
 		
 		try {
 			// Connection 객체 생성
-			conn = DriverManager.getConnection(url, user, password);
+//			conn = DriverManager.getConnection(url, user, password);
+			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, vo.getId());
 			pstmt.setString(2, vo.getPw());
@@ -78,7 +87,7 @@ public class UserDAO {
 		String sql = "select * from user where id = ? and pw = ?";
 		
 		try {
-			conn = DriverManager.getConnection(url, user, password);
+			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			pstmt.setString(2, pw);
@@ -110,7 +119,7 @@ public class UserDAO {
 		
 		try {
 			// conn객체 생성
-			conn = DriverManager.getConnection(url, user, password);
+			conn = ds.getConnection();
 			// pstmt 객체 생성
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
@@ -147,7 +156,7 @@ public class UserDAO {
 				+ "where id=?";
 		
 		try {
-			conn = DriverManager.getConnection(url, user, password);
+			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, vo.getName());
 			pstmt.setString(2, vo.getPhone1());
@@ -175,7 +184,7 @@ public class UserDAO {
 		String sql = "delete from user where id = ?";
 		
 		try {
-			conn = DriverManager.getConnection(url, user, password);
+			conn = ds.getConnection();
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			
